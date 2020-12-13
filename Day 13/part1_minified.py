3ab2a454e833252c10e22661ff74ba0c7ea870cc
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-13 22:02:25 +0100

    replaced semicolons with line breaks

--- Day 13/part1_minified.py
+++ Day 13/part1_minified.py
@@ -1,1 +1,4 @@
-t,i=open("input.txt").readlines();d={x:x-(int(t)%x)for x in(int(x)for x in i.split(',')if x!='x')};m=min(d,key=d.get);print(m*d[m])
+t,i=open("input.txt").readlines()
+d={x:x-(int(t)%x)for x in(int(x)for x in i.split(',')if x!='x')}
+m=min(d,key=d.get)
+print(m*d[m])
