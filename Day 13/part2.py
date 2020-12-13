e765c78804f52daa5b1224cb045f2582e27ed7f9
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-13 15:51:59 +0100

    day 13 brute force method

---
+++ Day 13/part2.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+lines = (x.strip() for x in open("input.txt"))
+
+next(lines)
+bus_ids = [id for id in next(lines).split(',')]
+bus_ids = [(int(bus),i) for (bus,i) in list(zip(bus_ids,range(len(bus_ids)))) if bus != 'x']
+
+headstart = 100000000000000
+
+i = headstart - (headstart % bus_ids[0][0])
+print(i % bus_ids[0][0])
+correct = True
+while True:
+    for bus in bus_ids:
+        if (i + bus[1]) % bus[0] != 0:
+            correct = False
+            break
+    if correct:
+        print('Solution: {}'.format(i))
+        break
+    else:
+        i = i + bus_ids[0][0]
+        correct = True
+    print(i)
