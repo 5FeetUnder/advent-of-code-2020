90783a4a5a6347a94df7a7d05fe527b8df048587
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-13 20:31:04 +0100

    moved minification to separate file

--- Day 13/part2_sequences.py
+++ Day 13/part2_sequences.py
@@ -1,14 +1,18 @@
 from math import gcd
-bus_ids = [(int(bus),-i) for (i,bus) in enumerate([x.strip() for x in open("D:/Users/Azure Meso/Documents/Programmierung/Advent of Code 2020/Day 13/input.txt")][1].split(',')) if bus != 'x']
+lines = [x.strip() for x in open("D:/Users/Azure Meso/Documents/Programmierung/Advent of Code 2020/Day 13/input.txt")]
+
+bus_ids = [id for id in lines[1].split(',')]
+bus_ids = [(int(bus),-i) for (bus,i) in list(zip(bus_ids,range(len(bus_ids)))) if bus != 'x']
 
 def kgv(x,y):
     return x * y // gcd(x, y)
 
 def combined_sequence(a, b, c, d):
     while b != d:
-        next_b_or_d = lambda u, v, w : u + ((v - u - 1) // w + 1) * w
-        b = next_b_or_d(b, d, a) if b < d else b
-        d = next_b_or_d(d, b, c) if d < b else d
+        if b < d:
+            b += ((d-b-1) // a + 1) * a
+        else:
+            d += ((b-d-1) // c + 1) * c
     return (kgv(a, c), b)
 
 solution = (1, 0)
