a4759c9f27591d8589f144d3e64080552ccf5401
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-13 21:58:52 +0100

    part 1 minified version 2

--- Day 13/part1_minified.py
+++ Day 13/part1_minified.py
@@ -1,7 +1,1 @@
-earliest,bus_ids = open("input.txt").readlines()
-earliest = int(earliest)
-bus_ids = [int(bus) for bus in bus_ids.split(',') if bus != 'x']
-
-time_to_busses = {bus:bus - (earliest%bus) for bus in bus_ids}
-
-print(min(time_to_busses, key=time_to_busses.get) * time_to_busses[min(time_to_busses, key=time_to_busses.get)])
+t,i=open("input.txt").readlines();t=int(t);d={x:x-(t%x)for x in(int(x)for x in i.split(',')if x!='x')};m=min(d,key=d.get);print(m*d[m])
