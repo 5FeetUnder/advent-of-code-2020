709b728cdea2b912ce8d3dfa210c1e7c195e5bdd
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-13 22:01:26 +0100

    part 1 minified version 3 t is now converted to in in place

--- Day 13/part1_minified.py
+++ Day 13/part1_minified.py
@@ -1,1 +1,1 @@
-t,i=open("input.txt").readlines();t=int(t);d={x:x-(t%x)for x in(int(x)for x in i.split(',')if x!='x')};m=min(d,key=d.get);print(m*d[m])
+t,i=open("input.txt").readlines();d={x:x-(int(t)%x)for x in(int(x)for x in i.split(',')if x!='x')};m=min(d,key=d.get);print(m*d[m])
