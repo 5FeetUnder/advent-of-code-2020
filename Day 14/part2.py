fff181756f8d607ce0bfb53fafc0c51e609f9e31
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-15 02:31:07 +0100

    compressed if in main

--- Day 14/part2.py
+++ Day 14/part2.py
@@ -25,8 +25,7 @@ class MemArray:
     
 mem = MemArray()
 for command,value in commands:
-    if command == 'mask':
-        mem.set_mask(value)
+    if command == 'mask': mem.set_mask(value)
     else:
         address = re.search(r"\[([\d]+)\]",command).group(1)
         mem.write_mem(int(address),int(value))
