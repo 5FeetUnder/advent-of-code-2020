64a4141511b0fd98a048c3563b3094257d72b29d
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-15 06:54:10 +0100

    Day 2 part 2: solved

---
+++ Day 2/part2.py
@@ -0,0 +1,9 @@
+import re
+
+# lines = ['1-3 a: abcde', '1-3 b: cdefg', '2-9 c: ccccccccc']
+lines = (x.strip() for x in open('Day 2/input.txt'))
+
+reg_ex = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
+
+valid = sum([1 if (x[3][int(x[0])-1] == x[2]) ^ (x[3][int(x[1])-1] == x[2]) else 0 for x in (x.groups() for x in (reg_ex.match(x) for x in lines))])
+print(valid)
