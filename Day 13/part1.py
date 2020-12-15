c252d175949296ae49dd631882caf7d0858090f5
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-15 02:23:43 +0100

    Day 13 part 1: replaced for loop with dictionary comprehension

--- Day 13/part1.py
+++ Day 13/part1.py
@@ -3,10 +3,7 @@ lines = (x.strip() for x in open("input.txt"))
 earliest = int(next(lines))
 bus_ids = [int(id) for id in next(lines).split(',') if id != 'x']
 
-time_to_busses = dict()
-
-for bus in bus_ids:
-    time_to_busses[bus] = bus - (earliest % bus)
+time_to_busses = {bus: bus - (earliest % bus) for bus in bus_ids}
 
 next_bus = min(time_to_busses, key=time_to_busses.get)
 print(next_bus * time_to_busses[next_bus])
