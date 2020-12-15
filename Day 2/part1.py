716c0f2aa5a2a8af146cca3ee2fc169ee1a90549
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-15 07:20:16 +0100

    removed unnecessary generator inception and unnecessary else in generator

--- Day 2/part1.py
+++ Day 2/part1.py
@@ -4,5 +4,5 @@ lines = (x.strip() for x in open('input.txt'))
 
 reg_ex = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
 
-valid = sum([1 if x[3].count(x[2]) <= int(x[1]) and x[3].count(x[2]) >= int(x[0]) else 0 for x in (x.groups() for x in (reg_ex.match(x) for x in lines))])
+valid = sum((1 for x in (reg_ex.match(x).groups() for x in lines) if x[3].count(x[2]) <= int(x[1]) and x[3].count(x[2]) >= int(x[0])))
 print(valid)
--- Day 2/part2.py
+++ Day 2/part2.py
@@ -4,5 +4,5 @@ lines = (x.strip() for x in open('input.txt'))
 
 reg_ex = re.compile(r"(\d+)-(\d+) (\w): (\w+)")
 
-valid = sum([1 if (x[3][int(x[0])-1] == x[2]) ^ (x[3][int(x[1])-1] == x[2]) else 0 for x in (x.groups() for x in (reg_ex.match(x) for x in lines))])
+valid = sum((1 for x in (reg_ex.match(x).groups() for x in lines) if (x[3][int(x[0]) - 1] == x[2]) ^ (x[3][int(x[1]) - 1] == x[2])))
 print(valid)
