ce26853b431fce45c46c60b7ef7c37cd7fd90fae
Benjamin Petsch <petschbenjamin@gmail.com>
2020-12-18 10:03:55 +0100

    day 18 solved

---
+++ day18/part1.py
@@ -0,0 +1,35 @@
+lines = ((x if x in ('*','+','(',')') else int(x) for x in line if x != ' ') for line in (x.strip() for x in open('input.txt')))
+ops = {
+    '+' : (lambda a, b: a + b),
+    '*' : (lambda a, b: a * b),
+}
+results = []
+for line in lines:
+    rpn = []
+    stack = []
+    for token in line:
+        if type(token) == int:
+            rpn.append(token)
+        elif token in ops:
+            while len(stack) > 0 and stack[-1] != '(':
+                rpn.append(stack.pop())
+            stack.append(token)
+        elif token == '(':
+            stack.append(token)
+        elif token == ')':
+            while stack[-1] != '(':
+                rpn.append(stack.pop())
+            stack.pop() if stack[-1] == '(' else None
+    while len(stack) > 0:
+        rpn.append(stack.pop())
+    if len(stack) > 0:
+        print('asd')
+    for token in rpn:
+        if token in ops:
+            b = stack.pop()
+            a = stack.pop()
+            stack.append(ops[token](a,b))
+        else:
+            stack.append(token)
+    results.append(stack.pop())
+print(sum(results))
